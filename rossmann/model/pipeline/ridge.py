c61f1c236f1bc71167a61181589b1793ea98b7bd
Maximilian Reimer <maximilian.reimer.42@gmail.com>
2022-07-25 15:11:19 +0200

    Added ridge training inclusive HP (alpha opt)

--- rossmann/model/pipeline/ridge.py
+++ rossmann/model/pipeline/ridge.py
@@ -1,3 +1,5 @@
+import argparse
+from pathlib import Path
 from sklearn.compose import ColumnTransformer  # type: ignore
 from sklearn.preprocessing import (  # type: ignore
     PowerTransformer,
@@ -12,15 +14,24 @@ from sklearn.pipeline import Pipeline  # type: ignore
 import pandas as pd
 
 from rossmann.model.pipeline import make_feature_extractor
+from joblib import dump  # type: ignore
+from rossmann.model.data_loader import load_instances_csv, load_stores_csv
+from rossmann.model.prepare_data import prepare_stores
+from rossmann.model.pipeline.filter import TopStoreSelector
+
+
+def to_binary_holidays(categorical_holidays: pd.Series) -> pd.Series:
+    return categorical_holidays != "no holiday"
 
 
 def make_feature_transform():
+
     feature_transform = ColumnTransformer(
         [
             ("unskew", PowerTransformer(), ["CompetitionDistance"]),
             (
                 "to_binary",
-                FunctionTransformer(lambda x: x != "no holiday"),
+                FunctionTransformer(to_binary_holidays),
                 ["StateHoliday"],
             ),
             (
@@ -73,3 +84,40 @@ def make_pipeline(prepared_stores: pd.DataFrame) -> Pipeline:
     )
 
     return pipeline
+
+
+def train(data_path: Path) -> Pipeline:
+    # todo split into train and main
+    # todo report progress / scores
+
+    train = load_instances_csv(data_path / "train.csv")
+    subset_train = TopStoreSelector(top_percent=0.1).fit_transform(train)
+    assert subset_train.notna().all().all()
+    X_train, y_train = (
+        subset_train.drop(columns=["Sales"]),
+        subset_train["Sales"],
+    )
+
+    stores = load_stores_csv(data_path / "store.csv")
+    prepared_stores = prepare_stores(train, stores)
+
+    # Promo2Since is NaTa if Promo2 False.
+    # That is ok, since this is only for lookup if Promo2 is True.
+    assert prepared_stores.drop("Promo2Since", axis=1).notna().all().all()
+
+    pipeline = make_pipeline(prepared_stores)
+    pipeline = pipeline.fit(X_train, y_train)
+
+    dump(pipeline, data_path / "ridge_model.pkl")
+
+
+def parse_args() -> argparse.Namespace:
+    # todo add help text
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=Path)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    pipeline = train(args.path)
